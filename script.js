f619077efc37b9c0559f7b42cef744884ce99017
Bo Zhu <zhubo03@gmail.com>
2011-11-24 18:51:49 -0500

    need to write js for fetch last update time

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function displayLastUpdateTime() {
+}
+
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-27264995-1']);
 _gaq.push(['_trackPageview']);
@@ -7,3 +10,19 @@ _gaq.push(['_trackPageview']);
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+function Caesar_decrypt(cipher) {
+    plain = "";
+
+    for (var i = 0; i < cipher.length; i++) {
+        plain += String.fromCharCode(cipher.charCodeAt(i) - 3);
+    }
+
+    return plain;
+}
+
+function displayEmailAddress() {
+    var ciphertext = 'er1}kxCxzdwhuorr1fd';
+    var plaintext = Caesar_decrypt(ciphertext);
+    document.getElementById('email_address').innerHTML = '<a rel="nofollow" href="mailto:bo.zhu@uwaterloo.ca">' + plaintext + '</a>';
+}
