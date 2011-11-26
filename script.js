f8e245989b3eb35bf2a00080ee34bf11e36f9b24
Bo Zhu <zhubo03@gmail.com>
2011-11-26 02:24:29 -0500

    finally all done. json-p is awesome! ie sucks

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
-function displayLastUpdateTime() {
-    if (window.XDomainRequest) {
-        var xdhttp = new XDomainRequest();
-        xdhttp.onload = function() {
-            alert(xdhttp.responseText);
-        }
-
-        xdhttp.onerror = function() {alert("error");}
-        xdhttp.ontimeout = function() {alert("timeout");}
-        xdhttp.onprogress = function() {alert("progress");}
-
-        xdhttp.open("GET", "http://github.com/api/v2/json/repos/show/bozhu/bozhu.github.com");
-        xdhttp.send();
-    } else {
-        var xmlhttp;
-        if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                var resp = eval ("(" + xmlhttp.responseText + ")");
-                var last_update_time = new Date(resp.updated_at);
-                document.getElementById('last_update_time').innerHTML = last_update_time.toLocaleDateString();
-            }
-        }
-        xmlhttp.open("GET", "https://api.github.com/repos/bozhu/bozhu.github.com", true);
-        xmlhttp.send();
-    }
+// to display last update time
+function displayLastUpdateTime(resp) {
+    var last_update_time = resp.data.updated_at;
+    document.getElementById('last_update_time').innerHTML = last_update_time.substring(0, 10);
 }
 
+(function() {
+    var github = document.createElement('script');
+    github.type = 'text/javascript';
+    github.async = true;
+    github.src = 'https://api.github.com/repos/bozhu/bozhu.github.com?callback=displayLastUpdateTime';
+    document.getElementsByTagName('body')[0].appendChild(github);
+})();
 
-// Google Analytics
+
+// for Google Analytics
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-27264995-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    var ga = document.createElement('script'); 
+    ga.type = 'text/javascript'; 
+    ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var s = document.getElementsByTagName('script')[0]; 
+    s.parentNode.insertBefore(ga, s);
 })();
 
 
+// to display email address
 function Caesar_decrypt(cipher) {
     plain = "";
     for (var i = 0; i < cipher.length; i++) {
