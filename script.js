7a90a8d83bbc34b60c273e75ad42c7bb4e997c31
Bo Zhu <zhubo03@gmail.com>
2012-05-31 21:51:02 -0400

    to use the google analytics on cloudflare, so remove the analytics js code from the script

--- script.js
+++ script.js
@@ -4,27 +4,13 @@ function displayLastUpdateTime(resp) {
     document.getElementById('last_update_time').innerHTML = last_update_time.substring(0, 10);
 }
 
+
 (function() {
     var github = document.createElement('script');
     github.type = 'text/javascript';
     github.async = true;
     github.src = 'https://api.github.com/repos/bozhu/bozhu.github.com?callback=displayLastUpdateTime';
     document.getElementsByTagName('body')[0].appendChild(github);
-})();
-
-
-// for Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-27264995-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script'); 
-    ga.type = 'text/javascript'; 
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; 
-    s.parentNode.insertBefore(ga, s);
 })();
 
 
